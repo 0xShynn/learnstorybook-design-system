07dc5ddbed01151697f7dc2c42698e0dfdc92870
“anhek” <a.nhek@icloud.com>
2021-07-20 15:53:52 +0200

    Improved AvatarList docs

--- src/AvatarList.js
+++ src/AvatarList.js
@@ -1,3 +1,7 @@
+/**
+ * A list of Avatars, ellipsized to at most 3. Supports passing only a subset of the total user count.
+ */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -77,7 +81,13 @@ export function AvatarList({ loading, users, userCount, size, ...props }) {
 }
 
 AvatarList.propTypes = {
+  /**
+   * Are we loading avatar data from the network?
+   */
   loading: PropTypes.bool,
+  /**
+   * A (sub)-list of the users whose avatars we have data for. Note: only 3 will be displayed.
+   */
   users: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
@@ -85,7 +95,13 @@ AvatarList.propTypes = {
       avatarUrl: PropTypes.string,
     })
   ),
+  /**
+   * The total number of users, if a subset is passed to `users`.
+   */
   userCount: PropTypes.number,
+  /**
+   * AvatarList comes in four sizes. In most cases, you’ll be fine with `medium`.
+   */
   size: PropTypes.oneOf(Object.keys(sizes)),
 };
 
